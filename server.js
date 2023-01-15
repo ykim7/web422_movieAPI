6c9e6be4d0ca4bf77b3f546ba8856a1e9e52d660
Yujin Kim <gettheradio@gmail.com>
2023-01-15 01:28:13 -0500

    add declaration

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+ *  WEB422 – Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *
+ *  Name: Yujin Kim  Student ID: 117826214  Date: January 15, 2023
+ *  Cyclic Link: https://friendly-gold-hem.cyclic.app/
+ *
+ ********************************************************************************/
+
 const HTTP_PORT = process.env.PORT || 8080;
 const express = require("express");
 const app = express();
